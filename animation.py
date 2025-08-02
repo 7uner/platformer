32b113ebcd89d9734799540b09d71eecb9dbd78d
Winston Zhu <winston.zhu@yahoo.ca>
2025-08-02 09:08:43 -0400

    added rocket animation

--- animation.py
+++ animation.py
@@ -12,17 +12,31 @@ pygame.display.set_caption("Blit Example")
 # Load image
 sprite_sheet = pygame.image.load(Path("images/nyan_cat.png").resolve().as_posix())
 
-#TODO:based on the new sprite size and loation, change the width and height appropriately
-frame_width = 150
-frame_height = 100
-#TODO: we need to calculate this y location based on where the top left corner of the sprite section is
-sprite_y_location = 0
-frames = []
-for i in range(6):
-    frame = sprite_sheet.subsurface(pygame.Rect(i * frame_width, sprite_y_location, frame_width, frame_height))
-    frames.append(frame)
+# sprite dimensions
+title_screen_frame_width = 150
+title_screen_frame_height = 100
+rocket_frame_width = 116.5
+rocket_frame_height = 50
 
-current_frame = 0
+title_screen_sprite_y_location = 0
+rocket_sprite_y_location = 265
+title_screen_frames = []
+rocket_frames = []
+
+for i in range(6):
+    frame = sprite_sheet.subsurface(pygame.Rect(i * title_screen_frame_width, title_screen_sprite_y_location,
+                                                title_screen_frame_width, title_screen_frame_height))
+    title_screen_frames.append(frame)
+
+for i in range(4):
+    print(i * rocket_frame_width, rocket_sprite_y_location,
+                                                rocket_frame_width, rocket_frame_height)
+    frame = sprite_sheet.subsurface(pygame.Rect(i * rocket_frame_width, rocket_sprite_y_location,
+                                                rocket_frame_width, rocket_frame_height))
+    rocket_frames.append(frame)
+
+title_screen_current_frame = 0
+rocket_current_frame = 0
 animation_speed = 0.1 # controls how fast the animation changes
 frame_timer = 0
 clock = pygame.time.Clock()
@@ -39,13 +53,16 @@ while running:
     frame_timer += dt
     if frame_timer >= animation_speed:
         frame_timer = 0
-        current_frame = (current_frame + 1) % len(frames)
+        #progress each sprite by 1 frame
+        title_screen_current_frame = (title_screen_current_frame + 1) % len(title_screen_frames)
+        rocket_current_frame = (rocket_current_frame + 1) % len(rocket_frames)
 
     # Fill screen with white color before bliting
     screen.fill((255, 255, 255))
 
     # Blit image at (100, 150)
-    screen.blit(frames[current_frame], (150, 100))
+    screen.blit(title_screen_frames[title_screen_current_frame], (50, 100))
+    screen.blit(rocket_frames[rocket_current_frame], (250, 100))
 
     # Update display
     pygame.display.flip()
