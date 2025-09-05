9c6e352c1f69f22b0aa2f5c9d2c8b7f46f44087c
Winston Zhu <winston.zhu@yahoo.ca>
2025-09-04 20:16:28 -0400

    added platform collisions

--- platformer.py
+++ platformer.py
@@ -18,7 +18,8 @@ background_absolute_width = 1200
 # variables
 player_x = WIDTH/2
 player_y = HEIGHT - 137
-player_y_speed = 0
+deltaY = 0
+deltaX = 0
 max_jump_height = HEIGHT - 137 - 160
 acceleration = 0
 scroll = 0
@@ -48,58 +49,93 @@ background = pygame.transform.scale(background, (background_absolute_width, HEIG
 clock = pygame.time.Clock()
 
 def handleInput():
-    global player_x, scroll, jump_state, number_of_jumps, player_y_speed, acceleration, running
+    global player_x, scroll, jump_state, number_of_jumps, deltaY, acceleration, running, deltaX
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
-        player.x -= 10
-        # if the player is on the right half of the screen, then anymore movement
-        # will push the background forward as well
-        if player.x < WIDTH/3:
-            scroll += 10
-            player.x += 10
-            for platform in platforms:
-                platform.x += 10
+        deltaX = -10
+
     if keys[pygame.K_RIGHT]:
-        player.x += 10
-        if player.x > WIDTH/3 * 2:
-            scroll -= 10
-            player.x -= 10
-            for platform in platforms:
-                platform.x -= 10
+        deltaX = 10
 
     if keys[pygame.K_SPACE]:
         if jump_state == 1 and number_of_jumps > 0:
-            player_y_speed = -10
+            deltaY = -10
             acceleration = 0.3
             jump_state = 2
             number_of_jumps -= 1
         elif (jump_state == 3) and number_of_jumps > 0:
-            player_y_speed = -10
+            deltaY = -10
             acceleration = 0.3
             jump_state = 2
             number_of_jumps -= 1
 
 
+def handleYCollision():
+    global platforms, player, deltaY, jump_state, number_of_jumps
+    for platform in platforms:
+        if player.colliderect(platform):
+            # falling down on to a platform
+            if deltaY > 0 and player.bottom <= platform.bottom:
+                player.bottom = platform.top
+                deltaY = 0
+                jump_state = 1
+                number_of_jumps = 2
+            # hitting underside of a platform
+            elif deltaY < 0:
+                player.top = platform.bottom
+                deltaY = 3
+
+
+def handleXCollision():
+    global platforms, player, deltaY, jump_state, number_of_jumps
+    for platform in platforms:
+        if player.colliderect(platform):
+            # hitting left side of a platform
+            if deltaX > 0:
+                player.right = platform.left
+            # hitting right side of a platform
+            elif deltaX < 0:
+                player.left = platform.right
+
+
 def update():
-    global jump_state, player, player_y_speed, acceleration, number_of_jumps
-    if jump_state == 2 and player_y_speed >= 0:
-        player_y_speed = 0
+    global jump_state, player, deltaY, acceleration, number_of_jumps, scroll, deltaX
+    # update jumps
+    if jump_state == 2 and deltaY >= 0:
+        deltaY = 0
         acceleration = 0.3
         jump_state = 3
     # if the player hits the ground, set speed back to 0
     if player.y > HEIGHT - 137:
         player.y = HEIGHT - 137
-        player_y_speed = 0
+        deltaY = 0
         acceleration = 0
         jump_state = 1
         number_of_jumps = 2
     # constantly update the speed
-    player_y_speed += acceleration
-    player.y += player_y_speed
+    player.x += deltaX
+    # if the player is on the right half of the screen, then anymore movement
+    # will push the background forward as well
+    if player.x < WIDTH / 3:
+        scroll -= deltaX
+        player.x -= deltaX
+        for platform in platforms:
+            platform.x -= deltaX
+    elif player.x > WIDTH/3 * 2:
+        scroll -= deltaX
+        player.x -= deltaX
+        for platform in platforms:
+            platform.x -= deltaX
+    handleXCollision()
+    
+    deltaY += acceleration
+    player.y += deltaY
+    handleYCollision()
+    deltaX = 0
 
 
 def draw():
